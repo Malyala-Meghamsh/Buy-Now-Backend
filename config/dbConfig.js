9968843c13b36d1873fd4e62109231e01fcdfdb9
Meghamsh <meghamalyala02@gmail.com>
2023-07-14 13:44:27 +0530

    db updated

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,2 +1,4 @@
+// require('dotnev').config();
+require('dotenv').config();
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/MarketPlaceDB");
+mongoose.connect(process.env.MONGO_URL);
