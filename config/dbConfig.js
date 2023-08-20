b007e48cfa63054f98470f62a5fa9b6ff0ea1445
Meghamsh <meghamalyala02@gmail.com>
2023-08-20 14:31:42 +0530

    Share feature added

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,4 +1,9 @@
 // require('dotnev').config();
 require('dotenv').config();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URL);
+// console.log(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    // console.log("Yass");
+}).catch(err=>{
+    console.log(err);
+});
